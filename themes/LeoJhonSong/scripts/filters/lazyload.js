28c62f6b3ed09fe2cbca6385a52418822bf7576e
Leo-AERO15WB <LeoJhon.Song@outlook.com>
2022-05-26 05:55:45 +0800

    update

--- themes/LeoJhonSong/scripts/filters/lazyload.js
+++ themes/LeoJhonSong/scripts/filters/lazyload.js
@@ -18,6 +18,12 @@ hexo.extend.filter.register('after_post_render', data => {
   images.each((i, o) => {
     let src = $(o).attr('src');
     $(o).attr('data-src', src).removeAttr('src');
+    // scale imgs by scaling factor after '?' in src
+    let fields = src.match(/(?<=\?)\d*/g);
+    if (fields && fields.length == 1) {
+      let scale = parseFloat(fields[0].toString());
+      $(o).attr('style', 'width: ' + scale + '%');
+    }
   });
 
   data.content = $.html();
